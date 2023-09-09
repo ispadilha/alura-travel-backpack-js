2feb215cb1ec8a8e65f13af9f4dee79ffeac465f
ispadilha <israel.padilha@gmail.com>
2023-09-09 16:26:19 -0300

    Created function to update existing item display

--- js/main.js
+++ js/main.js
@@ -12,14 +12,21 @@ form.addEventListener('submit', (evento) =>{
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
+    const exists = itens.find( elemento => elemento.nome === nome.value);
+
     const currentItem = {
         "nome": nome.value,
         "quantidade": quantidade.value,
     }
 
-    createElement(currentItem);
-
-    itens.push(currentItem);
+    if (exists){
+        currentItem.id = exists.id;
+        updatesElement(currentItem);
+    } else {
+        currentItem.id = itens.length;
+        createElement(currentItem);
+        itens.push(currentItem);
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -33,9 +40,14 @@ function createElement(item){
 
     const newItemNumber = document.createElement('strong');
     newItemNumber.innerHTML = item.quantidade;
+    newItemNumber.dataset.id = item.id;
 
     newItem.appendChild(newItemNumber);
     newItem.innerHTML += item.nome;
 
     lista.appendChild(newItem);
+}
+
+function updatesElement(item){
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
 }
